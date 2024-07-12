fix: remove normalization from njit dithering

dithering.py
@@ -71,15 +71,14 @@ def floyd_steinberg_dithering(img_arr, add_noise=True, palette=None):
 
 @njit
 def floyd_steinberg_dithering_njit(
-    img_arr: np.ndarray, palette: np.ndarray, add_noise:bool = True
-):
+    img_arr: np.ndarray, palette: np.ndarray
+) -> np.ndarray:
+    """
+    img_arr: The input image given as an array in range [0, 255]
+    """
     h, w = img_arr.shape
-    if add_noise:
-        noise = np.random.random_sample((h, w)) * NOISE_INTENSITY
-    else:
-        noise = np.zeros((h, w))
+    output = np.copy(img_arr).astype(np.float64)
 
-    output = np.copy(img_arr) + noise
     for j in range(h):
         for i in range(w):
             x = i if j % 2 == 0 else w - 1 - i
@@ -98,7 +97,7 @@ def floyd_steinberg_dithering_njit(
                 output[j + 1][x - 1] += error * 3 / 16
                 output[j + 1][x] += error * 5 / 16
                 output[j + 1][x - 1] += error * 1 / 16
-    return (np.clip(output, 0, 1) * 255).astype(np.uint8)
+    return (np.clip(output, 0, MAX_COLOR)).astype(np.uint8)
 
 
 def ordered_dithering(img_arr):

gb_dithering.py
@@ -4,7 +4,7 @@ import numpy as np
 import os.path
 
 # Local Modules
-from constants import MAX_COLOR, RGB_CHANNELS
+from constants import RGB_CHANNELS
 from dithering import floyd_steinberg_dithering_njit
 import utils
 from utils import scale_blerp_njit, scale_nn_njit
@@ -99,16 +99,13 @@ def main():
     # --------------------------------------------------------------------------
     # Normalize & dither
     print("Dithering")
-    normalized = grayscale.astype(float) / MAX_COLOR
     dithered_rgb = np.zeros(
         [total_frames, h1, w1, RGB_CHANNELS], dtype=np.uint8
     )
     dithered = np.zeros([total_frames, h1, w1], dtype=np.uint8)
-    for i, frame in enumerate(normalized):
+    for i, frame in enumerate(grayscale):
         # Use dithering to transform 256 grayscale to 4 colors grayscale
-        dithered[i] = floyd_steinberg_dithering_njit(
-            frame, GRAYSCALE_PALETTE, add_noise=False
-        )
+        dithered[i] = floyd_steinberg_dithering_njit(frame, GRAYSCALE_PALETTE)
         dithered_rgb[i] = np.stack([dithered[i]] * 3, axis=-1)
     iio.imwrite(DITHERED_FILENAME, dithered_rgb, fps=FPS)
 